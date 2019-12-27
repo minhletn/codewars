71e693d4dac173120b39caed533096d4f87d25b7
mikeminhle <mike.minhle@allocatesoftware.com>
2019-12-27 12:11:02 +1100

    solved Intervals

--- CodeWars/Intervals.cs
+++ CodeWars/Intervals.cs
@@ -1,27 +1,40 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public class Intervals {
 	public static int SumIntervals((int, int)[] intervals) {
+		//return intervals
+		//	.SelectMany(i => Enumerable.Range(i.Item1, i.Item2 - i.Item1))
+		//	.Distinct()
+		//	.Count();
+
 		int sum = 0;
 		List<(int, int)> mergedIntervals = new List<(int, int)>();
 
 		bool sameSize = false;
 
 		while (!sameSize) {
+			mergedIntervals.Clear();
+
 			foreach((int, int) pair in intervals) {
-				List<(int, int)> mergedPairs = new List<(int, int)>();
+				//List<(int, int)> mergedPairs = new List<(int, int)>();
 				bool hasOverlapped = false;
 
-				foreach((int, int) tracking in mergedIntervals) {
-					if(AreOverlapped(pair, (tracking.Item1, tracking.Item2)) || AreOverlapped((tracking.Item1, tracking.Item2), pair)) {
+				for(int i = 0; i < mergedIntervals.Count; i++) {
+					var tracking = mergedIntervals[i];
+
+					if(AreOverlapped(pair, (tracking.Item1, tracking.Item2))
+					|| AreOverlapped((tracking.Item1, tracking.Item2), pair)) {
 						hasOverlapped = true;
 
-						mergedPairs.Add(MergePair(tracking, pair));
+						mergedIntervals[i] = MergePair(tracking, pair);
+						break;
 					}
 				}
 
-				mergedIntervals.AddRange(hasOverlapped ? mergedPairs : new List<(int, int)>() { pair });
+				if(!hasOverlapped)
+					mergedIntervals.Add(pair);
 			}
 
 			sameSize = intervals.Length == mergedIntervals.Count;
@@ -45,20 +58,6 @@ public class Intervals {
 	/// <param name="pair2"></param>
 	/// <returns></returns>
 	private static (int, int) MergePair((int, int) pair1, (int, int) pair2) {
-		return (Min(pair1.Item1, pair2.Item1), Max(pair1.Item2, pair2.Item2));
-	}
-
-	private static int Min(int value1, int value2) {
-		if (value1 < value2)
-			return value1;
-
-		return value2;
-	}
-
-	private static int Max(int value1, int value2) {
-		if (value1 > value2)
-			return value1;
-
-		return value2;
+		return (Math.Min(pair1.Item1, pair2.Item1), Math.Max(pair1.Item2, pair2.Item2));
 	}
 }
